45c6e28ae3c6f220fdd39425b2d50469aa49b23f
Christina <zexian.wu@reece.com.au>
2020-05-24 15:47:39 +1000

    Initial backend set up

---
+++ userprojectservice-backend/src/main/java/edu/monash/userprojectservice/UserprojectserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.monash.userprojectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserprojectserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserprojectserviceApplication.class, args);
+	}
+
+}
