156719103fef98f08bffaf1a3f053c334a7bea37
qlyy0001 <qlyy0001@student.monash.edu>
2020-05-26 22:24:42 +1000

    adding remaining unstaged get and return project files

--- userprojectservice-backend/src/main/java/edu/monash/userprojectservice/service/ProjectService.java
+++ userprojectservice-backend/src/main/java/edu/monash/userprojectservice/service/ProjectService.java
@@ -1,10 +1,14 @@
 package edu.monash.userprojectservice.service;
 
+import edu.monash.userprojectservice.model.GetProjectResponse;
 import edu.monash.userprojectservice.repository.Project;
 import edu.monash.userprojectservice.repository.ProjectRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+
+import static org.springframework.http.HttpStatus.OK;
 
 import java.util.Optional;
 
@@ -15,18 +19,27 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public void getProject(int projectID) {
+    public ResponseEntity<GetProjectResponse> getProject(int projectID) {
         log.info("{\"message\":\"Getting project\", \"project\":\"{}\"}", projectID);
 
         // get from database
         Optional<Project> projectDetail = projectRepository.findbyProject(projectID);
 
         Project projectResponse = projectDetail.orElse(null);
-        if (projectResponse != null){
-            System.out.println(projectResponse.getProject_id());
-            System.out.println(projectResponse.getProject_name());
+        if (projectResponse == null){
+
+            System.out.println("Project does not exist.");
+            return new ResponseEntity<>(
+                    null, OK
+            );
         }
 
+        System.out.println(projectResponse.getProject_id());
+        System.out.println(projectResponse.getProject_name());
         log.info("{\"message\":\"Got project\", \"project\":\"{}\"", projectID);
+
+        return new ResponseEntity<GetProjectResponse>(
+                new GetProjectResponse(projectResponse.getProject_id(), projectResponse.getProject_name()), OK
+        );
     }
 }
