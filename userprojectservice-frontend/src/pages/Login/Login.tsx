795b9acc5993adae77acd4982828631ae1bfbffb
qlyy0001 <qlyy0001@student.monash.edu>
2020-06-02 22:17:08 +1000

    login now works :100:

--- userprojectservice-frontend/src/pages/Login/Login.tsx
+++ userprojectservice-frontend/src/pages/Login/Login.tsx
@@ -3,37 +3,40 @@ import "./Login.css"
 import { Page } from "../Page"
 import { GoogleLoginButton } from 'ts-react-google-login-component';
 
-const responseGoogle(googleUser: window.gapi.auth2.GooglUser) => {
-  console.log(response);
-  this.history.push("./project");
+// const responseGoogle(googleUser: window.gapi.auth2.GooglUser) => {
+//   console.log(response);
+//   this.history.push("./project");
+//}
+const preLoginTracking = () => {
+  console.log('Attemp to login with google');
 }
-// const preLoginTracking() => {
-//   console.log('Attemp to login with google');
-// }
-//
-// const errorHandler(error: string) => {
-//   // handle error if login got failed...
-//   console.error(error);
-// }
-//
-// const responseGoogle(googleUser: gapi.auth2.GoogleUser) => {
-//   const id_token = googleUser.getAuthResponse(true).id_token
-//   const googleId = googleUser.getId()
-//
-//   console.log({ googleId })
-//   console.log({accessToken: id_token})
-//   // Make user login in your system
-//   // login success tracking...
-// }
+
+const errorHandler = (error: string) => {
+  // handle error if login got failed...
+  console.error(error);
+}
+
+const responseGoogle = (googleUser: gapi.auth2.GoogleUser) => {
+  const id_token = googleUser.getAuthResponse(true).id_token
+  const googleId = googleUser.getId()
+
+  console.log({ googleId })
+  console.log({accessToken: id_token})
+  // Make user login in your system
+  // login success tracking...
+}
+
 const Login: Page = ({ integration, state, dispatch }) => {
+  const clientConfig = { client_id: '12178522373-cd2s7387k0kjtqf3tebfoahk82m6prt9.apps.googleusercontent.com' }
   return (
-    <GoogleLogin
-      clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy={"single_host_origin"}
-    />
+      <div>
+        <GoogleLoginButton
+            responseHandler={responseGoogle}
+            clientConfig={clientConfig}
+            preLogin={preLoginTracking}
+            failureHandler={errorHandler}
+        />
+      </div>
   )
 }
 
