9e2a1d2fd9a23188692e883e9ef030773713def1
Christina <zexian.wu@reece.com.au>
2020-06-02 22:35:50 +1000

    format the code

--- userprojectservice-frontend/src/pages/Login/Login.tsx
+++ userprojectservice-frontend/src/pages/Login/Login.tsx
@@ -1,42 +1,42 @@
 import React from "react"
 import "./Login.css"
 import { Page } from "../Page"
-import { GoogleLoginButton } from 'ts-react-google-login-component';
+import { GoogleLoginButton } from "ts-react-google-login-component"
 
 // const responseGoogle(googleUser: window.gapi.auth2.GooglUser) => {
 //   console.log(response);
 //   this.history.push("./project");
 //}
 const preLoginTracking = () => {
-  console.log('Attemp to login with google');
+  console.log("Attemp to login with google")
 }
 
 const errorHandler = (error: string) => {
   // handle error if login got failed...
-  console.error(error);
+  console.error(error)
 }
 
 const responseGoogle = (googleUser: gapi.auth2.GoogleUser) => {
   const id_token = googleUser.getAuthResponse(true).id_token
   //const googleId = googleUser.getId()
 
-  console.log( googleUser.getBasicProfile().getEmail())
-  console.log({accessToken: id_token})
+  console.log(googleUser.getBasicProfile().getEmail())
+  console.log({ accessToken: id_token })
   // Make user login in your system
   // login success tracking...
 }
 
 const Login: Page = ({ integration, state, dispatch }) => {
-  const clientConfig = { client_id: '12178522373-cd2s7387k0kjtqf3tebfoahk82m6prt9.apps.googleusercontent.com' }
+  const clientConfig = { client_id: "12178522373-cd2s7387k0kjtqf3tebfoahk82m6prt9.apps.googleusercontent.com" }
   return (
-      <div>
-        <GoogleLoginButton
-            responseHandler={responseGoogle}
-            clientConfig={clientConfig}
-            preLogin={preLoginTracking}
-            failureHandler={errorHandler}
-        />
-      </div>
+    <div>
+      <GoogleLoginButton
+        responseHandler={responseGoogle}
+        clientConfig={clientConfig}
+        preLogin={preLoginTracking}
+        failureHandler={errorHandler}
+      />
+    </div>
   )
 }
 
