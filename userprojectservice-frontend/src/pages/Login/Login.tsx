da66e2ee27ac7bea90a4e94047ef3b360296baa6
Christina <zexian.wu@reece.com.au>
2020-06-02 23:19:42 +1000

    Redirect when click login button

--- userprojectservice-frontend/src/pages/Login/Login.tsx
+++ userprojectservice-frontend/src/pages/Login/Login.tsx
@@ -3,31 +3,32 @@ import "./Login.css"
 import { Page } from "../Page"
 import { GoogleLoginButton } from "ts-react-google-login-component"
 
-// const responseGoogle(googleUser: window.gapi.auth2.GooglUser) => {
-//   console.log(response);
-//   this.history.push("./project");
-//}
 const preLoginTracking = () => {
   console.log("Attemp to login with google")
 }
 
 const errorHandler = (error: string) => {
-  // handle error if login got failed...
   console.error(error)
 }
 
 const responseGoogle = (googleUser: gapi.auth2.GoogleUser) => {
   const id_token = googleUser.getAuthResponse(true).id_token
-  //const googleId = googleUser.getId()
+  const googleId = googleUser.getId()
 
+  console.log({ googleId })
   console.log(googleUser.getBasicProfile().getEmail())
   console.log({ accessToken: id_token })
-  // Make user login in your system
-  // login success tracking...
 }
 
+export type uxMode = "redirect" | "popup" | undefined
+
 const Login: Page = ({ integration, state, dispatch }) => {
-  const clientConfig = { client_id: "12178522373-cd2s7387k0kjtqf3tebfoahk82m6prt9.apps.googleusercontent.com" }
+  const clientConfig = {
+    client_id: "12178522373-cd2s7387k0kjtqf3tebfoahk82m6prt9.apps.googleusercontent.com",
+    ux_mode: "redirect" as uxMode,
+    redirect_uri: "http://localhost:3000/projects"
+  }
+
   return (
     <div>
       <GoogleLoginButton
