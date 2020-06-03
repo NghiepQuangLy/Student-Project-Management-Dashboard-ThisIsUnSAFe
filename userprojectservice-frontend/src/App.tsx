8bb093247994e8fe3e57fbd4904defb17969121c
Christina <zexian.wu@reece.com.au>
2020-06-03 13:05:23 +1000

    Change to use react router

--- userprojectservice-frontend/src/App.tsx
+++ userprojectservice-frontend/src/App.tsx
@@ -1,12 +1,31 @@
 import React, { FunctionComponent, useReducer } from "react"
-import styles from "./App.module.css"
 import { Integration } from "./integrations/Integration"
-import routeConfig from "./routeConfig"
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom"
+import Login from "./pages/Login/Login"
 import AppReducer from "./state/AppReducer"
 import AppInitialState from "./state/AppState"
-import { navigate, setBasepath, useRoutes } from "hookrouter"
+// import { setBasepath } from "hookrouter"
+// import { Route } from "react-router-dom"
+import ProjectList from "./pages/ProjectList/ProjectList"
+// import Login from "./pages/Login/Login"
+//
+// setBasepath(process.env.REACT_APP_CONTEXT_PATH || "")
+//
 
-setBasepath(process.env.REACT_APP_CONTEXT_PATH || "")
+// const App: FunctionComponent<AppProps> = ({ integration }) => {
+//   // const { page: CurrentRoute } = useRoutes(routeConfig)
+//   // const [state, dispatch] = useReducer(AppReducer, AppInitialState)
+//
+//   return (
+//     <div>
+//       <Route exact path="/" component={Login} />
+//       <Route exact path="/projects" component={ProjectList} />
+//     </div>
+//   )
+// }
+//
+// export default App
+//
 
 interface AppProps {
   integration: Integration
@@ -14,14 +33,49 @@ interface AppProps {
 }
 
 const App: FunctionComponent<AppProps> = ({ integration }) => {
-  const { page: CurrentRoute } = useRoutes(routeConfig)
+  // const { page: CurrentRoute } = useRoutes(routeConfig)
   const [state, dispatch] = useReducer(AppReducer, AppInitialState)
-
   return (
-    <main className={styles.Main}>
-      {(CurrentRoute && <CurrentRoute integration={integration} state={state} dispatch={dispatch} />) || navigate("/", true)}
-    </main>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/projects">projects</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL.
+            REMEMBER to have longer path at the top
+        */}
+        <Switch>
+          <Route path="/projects">
+            <ProjectList integration={integration} state={state} dispatch={dispatch} />
+          </Route>
+          <Route path="/">
+            <Login integration={integration} state={state} dispatch={dispatch} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
+
+// function Home() {
+//   return <h2>Home</h2>
+// }
+//
+// function About() {
+//   return <h2>About</h2>
+// }
+//
+// function Users() {
+//   return <h2>Users</h2>
+// }
 
 export default App
