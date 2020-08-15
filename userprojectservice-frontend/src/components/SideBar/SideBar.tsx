b26bb32458b02ce278b3085ed5af6ed6df1703f3
Sriram Viswanathan <svis0004@student.monash.edu>
2020-08-15 18:21:44 +1000

    Updated Sidebar with the correct sidebar items
    Updated Sidebar with the correct sidebar items

--- userprojectservice-frontend/src/components/SideBar/SideBar.tsx
+++ userprojectservice-frontend/src/components/SideBar/SideBar.tsx
@@ -4,37 +4,38 @@ import { BarContainerContext } from "../BarContainer/BarContainer"
 import styles from "./SideBar.module.css"
 
 interface SideBarItemProps {
-  itemName: string
-  itemLink: string
+    itemName: string
+    itemLink: string
 }
 
 const SideBarItem: FunctionComponent<SideBarItemProps> = ({ itemName, itemLink }) => {
-  return (
-    <li>
-      <a href={itemLink}>{itemName}</a>
-    </li>
-  )
+    return (
+        <li>
+            <a href={itemLink}>{itemName}</a>
+        </li>
+    )
 }
 
 const SideBar = () => {
-  const { isShowSidebar, setIsShowSidebar } = useContext(BarContainerContext)
-  const sidebarStyle = isShowSidebar ? styles.SideBarShow : styles.SideBarHide
-  return (
-    <div className={[styles.SideBar, sidebarStyle].join(" ")}>
-      <div className={styles.TopWrapper}>
-        <BurgerButton onClick={() => setIsShowSidebar(false)} />
-      </div>
-      <ul className={styles.MenuWrapper}>
-        <SideBarItem itemLink={"#"} itemName={"Project Name"} />
-        <SideBarItem itemLink={"#"} itemName={"Reminders"} />
-        <SideBarItem itemLink={"#"} itemName={"Project Problems"} />
-        <SideBarItem itemLink={"#"} itemName={"Export Data"} />
-        <SideBarItem itemLink={"#"} itemName={"Time Tracking"} />
-        <SideBarItem itemLink={"#"} itemName={"Contacts"} />
-        <SideBarItem itemLink={"#"} itemName={"Sign out"} />
-      </ul>
-    </div>
-  )
+    const { isShowSidebar, setIsShowSidebar } = useContext(BarContainerContext)
+    const sidebarStyle = isShowSidebar ? styles.SideBarShow : styles.SideBarHide
+    return (
+        <div className={[styles.SideBar, sidebarStyle].join(" ")}>
+            <div className={styles.TopWrapper}>
+                <BurgerButton onClick={() => setIsShowSidebar(false)} />
+            </div>
+            <ul className={styles.MenuWrapper}>
+                <SideBarItem itemLink={"#"} itemName={"Dashboard"} />
+                <SideBarItem itemLink={"#"} itemName={"Integrations"} />
+                <SideBarItem itemLink={"#"} itemName={"Reminders"} />
+                <SideBarItem itemLink={"#"} itemName={"Project Problems"} />
+                <SideBarItem itemLink={"#"} itemName={"Export Data"} />
+                <SideBarItem itemLink={"#"} itemName={"Time Tracking"} />
+                <SideBarItem itemLink={"#"} itemName={"Contacts"} />
+                <SideBarItem itemLink={"#"} itemName={"Sign out"} />
+            </ul>
+        </div>
+    )
 }
 
 export default SideBar
