1f179dc15c9802372532db045058dea3be12bf2c
Sriram Viswanathan <svis0004@student.monash.edu>
2020-08-16 18:43:02 +1000

    Added Sign out functionality
    Added Sign out functionality and redirect the application to the log in landing page

--- userprojectservice-frontend/src/components/TopBar/TopBar.tsx
+++ userprojectservice-frontend/src/components/TopBar/TopBar.tsx
@@ -2,10 +2,16 @@ import React, { FunctionComponent, useContext } from "react"
 import { BarContainerContext } from "../BarContainer/BarContainer"
 import BurgerButton from "../BurgerButton/BurgerButton"
 import styles from "./TopBar.module.css"
+import { GoogleLogout } from 'react-google-login';
+
 
 interface TopBarProps {
   shouldContainSideBar: Boolean
   pageTitle: string
+}
+
+const responseGoogle = () => {
+  window.location.href = "/"
 }
 
 const TopBar: FunctionComponent<TopBarProps> = ({ shouldContainSideBar, pageTitle }) => {
@@ -14,9 +20,17 @@ const TopBar: FunctionComponent<TopBarProps> = ({ shouldContainSideBar, pageTitl
     <div className={styles.LeftSideBar__TopSection}>
       {shouldContainSideBar && <BurgerButton onClick={() => setIsShowSidebar(true)} />}
       <div className={styles.pageTitle}>{pageTitle}</div>
-      <a className={styles.logout} href="#">
-        Sign Out
-      </a>
+
+      <GoogleLogout
+        clientId="12178522373-e5nmdu6ogip7e70f2sn645j30n55fgke.apps.googleusercontent.com"
+        buttonText="Logout"
+        onLogoutSuccess={responseGoogle}
+
+        render={renderProps => (
+          <a onClick={renderProps.onClick} className={styles.logout}>Log Out</a>
+        )}
+      >
+      </GoogleLogout>
     </div>
   )
 }
