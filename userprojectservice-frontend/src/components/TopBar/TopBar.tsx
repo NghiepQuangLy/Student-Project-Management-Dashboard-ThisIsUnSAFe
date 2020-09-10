3127ef25d48d047b2e672293027978de91e62de0
Sriram Viswanathan <svis0004@student.monash.edu>
2020-09-10 23:37:49 +1000

    Added Project list navigation on the top bar
    Added Project list navigation on the top bar to go back to the projects list page

--- userprojectservice-frontend/src/components/TopBar/TopBar.tsx
+++ userprojectservice-frontend/src/components/TopBar/TopBar.tsx
@@ -3,16 +3,20 @@ import "antd/dist/antd.css"
 import { Layout } from "antd"
 import styles from "./TopBar.module.css"
 import { useGoogleAuth } from "../GoogleAuthProvider/GoogleAuthProvider"
+import { Link } from "react-router-dom"
 
-interface TopBarProps {}
+interface TopBarProps { }
 
 const { Header } = Layout
 
-const TopBar: FunctionComponent<TopBarProps> = ({}) => {
+const TopBar: FunctionComponent<TopBarProps> = ({ }) => {
   const { signOut } = useGoogleAuth()
   return (
     <Header className={styles.TopBar}>
       <div className={styles.Logo} />
+      <Link to={{ pathname: "/projects" }} className={styles.Projects}>
+        Projects
+      </Link>
       <button className={styles.Logout} onClick={signOut}>
         Log Out
       </button>
