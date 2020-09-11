4c199148bdac2b03d7a04df4fdff3d04aa0f94f9
Christina <zexian.wu@reece.com.au>
2020-09-11 17:03:26 +1000

    Preparing for integrate with the other team

--- userprojectservice-frontend/src/components/ProjectDetailsIntegration/ProjectDetailsIntegration.tsx
+++ userprojectservice-frontend/src/components/ProjectDetailsIntegration/ProjectDetailsIntegration.tsx
@@ -1,16 +1,76 @@
 import React, { FunctionComponent } from "react"
 import styles from "./ProjectDetailsIntegration.module.css"
+import {
+  GIT_ID_QUERY,
+  GOOGLE_DRIVE_ID_QUERY,
+  PROJECT_DETAIL_CONSTRACTS_PATH,
+  PROJECT_DETAIL_EXPORT_DATA_PATH,
+  PROJECT_DETAIL_GIT_PATH,
+  PROJECT_DETAIL_GOOGLE_DRIVE_PATH,
+  PROJECT_DETAIL_PROJECT_PROBLEMS_PATH,
+  PROJECT_DETAIL_REMINDERS_PATH,
+  PROJECT_DETAIL_TIME_TRACKING_PATH,
+  PROJECT_DETAIL_TRELLO_PATH,
+  PROJECT_ID_QUERY,
+  TRELLO_ID_QUERY,
+  useQuery
+} from "../../util/useQuery"
 
 interface ProjectDetailsIntegrationProps {}
 
 const ProjectDetailsIntegration: FunctionComponent<ProjectDetailsIntegrationProps> = () => {
-  const currentPath = window.location.pathname
-  console.log(currentPath)
+  let query = useQuery()
+  let url = ""
+  const projectId = query?.get(PROJECT_ID_QUERY)
 
-  // will change to different path when doing actual integration with the other team
+  let integrationIdQuery
+
+  switch (window.location.pathname) {
+    case PROJECT_DETAIL_GIT_PATH:
+      const gitId = query?.get(GIT_ID_QUERY)
+      integrationIdQuery = gitId ? `&git-id=${gitId}` : ""
+      url = `${process.env.REACT_APP_GIT_URL}?project-id=${projectId}${integrationIdQuery}`
+      console.log(url)
+      break
+    case PROJECT_DETAIL_TRELLO_PATH:
+      const trelloId = query?.get(TRELLO_ID_QUERY)
+      integrationIdQuery = trelloId ? `&trello-id=${trelloId}` : ""
+      url = `${process.env.REACT_APP_TRELLO_URL}?project-id=${projectId}${integrationIdQuery}`
+      console.log(url)
+      break
+    case PROJECT_DETAIL_GOOGLE_DRIVE_PATH:
+      const driveId = query?.get(GOOGLE_DRIVE_ID_QUERY)
+      integrationIdQuery = driveId ? `&drive-id=${driveId}` : ""
+      url = `${process.env.REACT_APP_GOOGLE_DRIVE_URL}?project-id=${projectId}${integrationIdQuery}`
+      console.log(url)
+      break
+    case PROJECT_DETAIL_REMINDERS_PATH:
+      url = `${process.env.REACT_APP_REMINDER_URL}?project-id=${projectId}`
+      console.log(url)
+      break
+    case PROJECT_DETAIL_PROJECT_PROBLEMS_PATH:
+      url = `${process.env.REACT_APP_PROJECT_PROBLEM_URL}?project-id=${projectId}`
+      console.log(url)
+      break
+    case PROJECT_DETAIL_EXPORT_DATA_PATH:
+      url = `${process.env.REACT_APP_EXPORT_DATA_URL}?project-id=${projectId}`
+      console.log(url)
+      break
+    case PROJECT_DETAIL_TIME_TRACKING_PATH:
+      url = `${process.env.REACT_APP_TIME_TRACKING_URL}?project-id=${projectId}`
+      console.log(url)
+      break
+    case PROJECT_DETAIL_CONSTRACTS_PATH:
+      url = `${process.env.REACT_APP_CONTACTS_URL}?project-id=${projectId}`
+      console.log(url)
+      break
+    default:
+      break
+  }
+
   return (
     <div>
-      <iframe className={styles.Iframe} src="http://localhost:3000/Example" scrolling={"no"} title={"integrationIframe"} />
+      <iframe className={styles.Iframe} src={url} scrolling={"no"} title={"integrationIframe"} />
     </div>
   )
 }
--- userprojectservice-frontend/src/pages/Example/Example.tsx
+++ userprojectservice-frontend/src/pages/Example/Example.tsx
@@ -5,7 +5,7 @@ import styles from "./Example.module.css"
 const Example: Page = ({ integration, state, dispatch }) => {
   return (
     <div className={styles.Example}>
-      <div className={styles.ExampleHeader}>Git Name</div>
+      <div className={styles.ExampleHeader}>Example Header</div>
       <div className={styles.ExampleCharts}>
         <div className={styles.ExmapleChart}>Git Chart1</div>
         <div className={styles.ExmapleChart}>Git Chart2</div>
