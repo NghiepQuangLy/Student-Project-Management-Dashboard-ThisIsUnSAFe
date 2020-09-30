3bf61e9c99ee116eb93ace9842443266e336ca3c
giahuytruong <giahuy_truong@live.com.au>
2020-09-30 20:21:17 +1000

    Formatted new code
    Removed unnecessary imports and typo mistakes.

--- userprojectservice-backend/src/main/java/edu/monash/userprojectservice/repository/EditProjectRepository.java
+++ userprojectservice-backend/src/main/java/edu/monash/userprojectservice/repository/EditProjectRepository.java
@@ -1,21 +1,13 @@
 package edu.monash.userprojectservice.repository;
 
-//import edu.monash.userprojectservice.repository.project.ProjectEntity;
-//import edu.monash.userprojectservice.repository.project.ProjectsRepository;
-//import edu.monash.userprojectservice.repository.user.UsersRepository;
-//import edu.monash.userprojectservice.repository.userproject.UsersProjectsEntity;
-//import edu.monash.userprojectservice.repository.userproject.UsersProjectsRepository;
 import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-//import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
-import java.util.List;
 
 @Slf4j
 @Repository
@@ -34,19 +26,20 @@ public class EditProjectRepository {
         try {
             conn.setAutoCommit(false);
 
-            // create the java mysql update preparedstatement
+            // create the java mysql update prepared statement
             String query = "update PROJECTS set project_name = ? where project_id = ?";
             PreparedStatement preparedStmt = conn.prepareStatement(query);
             preparedStmt.setString   (1, newProjectName);
             preparedStmt.setString(2, projectId);
 
-            // execute the java preparedstatement
+            // execute the java prepared statement
             preparedStmt.executeUpdate();
 
             conn.commit();
             return true;
         } catch (SQLException e) {
             // in case of exception, rollback the transaction
+
             log.warn(String.valueOf(e));
             conn.rollback();
             return false;
