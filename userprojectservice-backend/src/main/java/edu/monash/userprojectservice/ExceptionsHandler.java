994e8a33e2fcd1ea7a52e6126abd5142f0d0e323
Christina <zexian.wu@reece.com.au>
2020-10-23 01:22:00 +1100

    Create exception handler to handle error that fail by @Valid

---
+++ userprojectservice-backend/src/main/java/edu/monash/userprojectservice/ExceptionsHandler.java
@@ -0,0 +1,32 @@
+package edu.monash.userprojectservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+@Component
+public class ExceptionsHandler {
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<String> handle(MethodArgumentNotValidException exception) {
+        //do your stuff here
+        System.out.println(exception.getBindingResult().getFieldErrors());
+        return exception.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fieldError ->  fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.toList());
+    }
+}
