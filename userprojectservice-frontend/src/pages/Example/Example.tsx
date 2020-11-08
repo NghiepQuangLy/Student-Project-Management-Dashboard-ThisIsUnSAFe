9e5615f1089d539bd71169d1befdf26d3661e9fe
qlyy0001 <qlyy0001@student.monash.edu>
2020-11-08 23:31:26 +1100

    Added top bar to the Safe and Agile Feature page for easy navigation

--- userprojectservice-frontend/src/pages/Example/Example.tsx
+++ userprojectservice-frontend/src/pages/Example/Example.tsx
@@ -1,20 +1,23 @@
 import React from "react"
 import { Page } from "../Page"
 import styles from "./Example.module.css"
+import BarContainer from "../../components/BarContainer/BarContainer";
 
 const Example: Page = ({ integration, state, dispatch }) => {
   return (
-    <div className={styles.Example}>
-      <div className={styles.ExampleHeader}>Example Header</div>
-      <div className={styles.ExampleCharts}>
-        <div className={styles.ExmapleChart}>Git Chart1</div>
-        <div className={styles.ExmapleChart}>Git Chart2</div>
-        <div className={styles.ExmapleChart}>Git Chart3</div>
-      </div>
-      <div className={styles.ExmapleContents}>
-        <div className={styles.ExmapleContent}>Git Content</div>
-      </div>
-    </div>
+      <BarContainer shouldContainSideBar={false}>
+        <div className={styles.Example}>
+          <div className={styles.ExampleHeader}>Example Header</div>
+          <div className={styles.ExampleCharts}>
+            <div className={styles.ExmapleChart}>Git Chart1</div>
+            <div className={styles.ExmapleChart}>Git Chart2</div>
+            <div className={styles.ExmapleChart}>Git Chart3</div>
+          </div>
+          <div className={styles.ExmapleContents}>
+            <div className={styles.ExmapleContent}>Git Content</div>
+          </div>
+        </div>
+      </BarContainer>
   )
 }
 
